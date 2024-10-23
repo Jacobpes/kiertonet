04941a38467a45189e8f794c79d27977200cf1ca
Jacobpes <jacob.pesamaa@gmail.com>
2024-10-23 19:19:21 +0300

    chore: Refactor main.py for readability and add email notification for new items

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
 ### Search Agent for kiertonet.fi
 
-* How to use:
+## Use the search agent with your own search query:
 1. Install dependencies: `pip install -r requirements.txt`
 2. Search for what you want on kiertonet.fi
 3. Inspect the page and find the url of the search results page
 4. Copy the url and paste it in the beginning in main.py to the URL_SEARCHAGENT variable
-5. Run the script: `python main.py`
-6. Let the script run in the background and enjoy knowing about the new items!
+5. Set your smtp server details in the .env file, you can see in the main.py file the variables you need to set.
+6. Run the script: `python main.py`
+7. Let the script run in the background and enjoy knowing about the new items!
--- main.py
+++ main.py
@@ -6,9 +6,10 @@ import time
 import os
 from dotenv import load_dotenv
  
-load_dotenv()
-
 URL_SEARCHAGENT = "https://kiertonet.fi/filter-auctions?page=1&search=teklab&hide_ended=0&only_sold_to_highest=0&iframe=false&scope=&scope_param="
+REQUEST_INTERVAL = 60 # seconds
+
+load_dotenv()
 
 # Email configuration
 SMTP_SERVER = os.getenv('SMTP_SERVER')
@@ -16,6 +17,7 @@ SMTP_PORT = os.getenv('SMTP_PORT')
 SENDER_EMAIL = os.getenv('SENDER_EMAIL')
 SENDER_PASSWORD = os.getenv('SENDER_PASSWORD')
 RECEIVER_EMAIL = os.getenv('RECEIVER_EMAIL')
+
 # Print out the environment variables to debug
 print(f"SMTP_SERVER: {SMTP_SERVER}")
 print(f"SMTP_PORT: {SMTP_PORT}")
@@ -68,51 +70,58 @@ def send_email(subject, body):
         return False
 
 
-def searchAgent():
+def fetch_data():
     response_json = requests.get(URL_SEARCHAGENT)
-    data = response_json.json().get('data')
+    return response_json.json().get('data')
+
+def process_item(item):
+    interesting_data = ['id', 'title', 'highest_bid', 'is_sold_to_highest_bidder', 'fullUrl']
+    item_details = []
+    for article in interesting_data:
+        if article == 'fullUrl': 
+            print(article, ":", "\033[94m" + item.get(article) + "\033[0m")
+            item_details.append(f"{article}: {item.get(article)}")
+        else:
+            item_details.append(f"{article}: {item.get(article)}")
+    return item_details
+
+def handle_new_item(item, index):
+    # Ensure item_id_memory is initialized
+    global item_id_memory
+    if 'item_id_memory' not in globals():
+        item_id_memory = []
+
+    print("""--------------------------------
+        SEARCH AGENT FOUND NEW ITEMS
+        """)
+    print("ITEM", index + 1)
+    print("item", item)
+
+    item_details = process_item(item)
+    subject = "New Item Found by Search Agent" + item.get('title') + " - " + item.get('fullUrl')
+    body = "\n".join(item_details)
+
+    success = send_email(subject, body)
+    if success:
+        item_id_memory.append(str(item.get('id')))
+        item_id_memory = list(set(item_id_memory))
+        with open('item_id_memory.txt', 'w') as file:
+            file.write(','.join(map(str, item_id_memory)))
+        print("Email sent successfully!")
+
+def searchAgent():
+    data = fetch_data()
     print("found", len(data), "items that match the search!")
     new_item = False
     for index, item in enumerate(data):
-        # get item_id and convert it to string
         item_id = str(item.get('id'))
         if item_id not in item_id_memory:
             new_item = True
-            print("""--------------------------------
-                SEARCH AGENT FOUND NEW ITEMS
-                """)
-
-            print("ITEM", index + 1)
-            print("item", item)
-
-            interesting_data = ['id', 'title', 'highest_bid', 'is_sold_to_highest_bidder', 'fullUrl']
-            item_details = []
-            for article in interesting_data:
-                if article == 'fullUrl': 
-                    # print link in blue
-                    print(article, ":", "\033[94m" + item.get(article) + "\033[0m")
-                    item_details.append(f"{article}: {item.get(article)}")
-                else:
-                    item_details.append(f"{article}: {item.get(article)}")
-
-            # Prepare email content
-            subject = "New Item Found by Search Agent" + item.get('title') + " - " + item.get('fullUrl')
-            body = "\n".join(item_details)
-
-            # Send the email
-            success = send_email(subject, body)
-            if success:
-                item_id_memory.append(str(item.get('id')))
-                # write item_id_memory to file
-                with open('item_id_memory.txt', 'w') as file:
-                    file.write(','.join(map(str, item_id_memory)))
-                # close file
-                file.close()
-                print("Email sent successfully!")
-    if new_item == False:
+            handle_new_item(item, index)
+    if not new_item:
         print("No new items found")
 
 # Set timeout to call searchAgent every minute to see if there are new items
 while True:
     searchAgent()
-    time.sleep(600) # wait 10 minutes before calling searchAgent again
+    time.sleep(REQUEST_INTERVAL) # wait 10 minutes before calling searchAgent again
